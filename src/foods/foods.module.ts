dd4d2ad13b58f8f33be810247b35a9f0544c85cc
zzaizzai <junsai0530@gmail.com>
2024-05-11 13:56:11 +0900

    remove prisma

--- src/foods/foods.module.ts
+++ src/foods/foods.module.ts
@@ -3,7 +3,6 @@ import { FoodsService } from './foods.service';
 import { FoodsController } from './foods.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Food } from './entities/food.entity';
-import { FoodsRepository } from './foods.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Food])],
