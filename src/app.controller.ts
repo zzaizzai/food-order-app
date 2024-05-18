a2661850eb9df0ffafc3f077d6babfafd4fa8580
zzaizzai <junsai0530@gmail.com>
2024-05-19 04:57:19 +0900

    ejs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/index')
+  @Render('index')
+  indexPage(@Res() res: Response) {
+    return { msg: "hello" }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config'
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const serverConfig = config.get('server')
 
   const port = serverConfig.port;
+
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.setBaseViewsDir(join(__dirname, '..', 'views'))
+  app.setViewEngine('ejs')
+  
   await app.listen(port);
 
   Logger.log(`Application running on port ${port}`)
